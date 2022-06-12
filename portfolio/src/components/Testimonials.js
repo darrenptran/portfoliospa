28f355aebc9dc20a9a5b8c4c0664ceb4e6cfddfa
Darren P. Tran <darrenptran@gmail.com>
2022-06-11 19:19:29 -0700

    fc

---
+++ portfolio/src/components/Testimonials.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+// import { testimonials } from "../data";
+
+export default function Testimonials() {
+  return (
+    <section id="testimonials">
+      <div className="container px-5 py-10 mx-auto text-center">
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          Client Testimonials
+        </h1>
+        <div className="flex flex-wrap m-4">
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
+                <div className="inline-flex items-center">
+                </div>
+              </div>
+            </div>
+          )
+        </div>
+      </div>
+    </section>
+  );
+}
